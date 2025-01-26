ca422c9b6437dd6e8f4ef0ab29d4f055f53e725c
tusherbhakta <tusheranta@gmail.com>
2025-01-26 23:27:22 +0530

    add initial React and Vite setup with Tailwind CSS integration

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-700'>
+        hii
+
+        <button className='btn btn-primary'>Login</button>
+      </div>
+    </>
+  )
+}
+
+export default App
