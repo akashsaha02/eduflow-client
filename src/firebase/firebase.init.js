2aac83cd2065e19f9fd3040f45f37c68eb346ddf
tusherbhakta <tusheranta@gmail.com>
2025-01-26 22:11:31 +0530

    implement Firebase authentication and setup environment variables

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD4IRRI1PBsnWxmjzPjZRD7r-xpoc5rGzg",
-  authDomain: "academix-89443.firebaseapp.com",
-  projectId: "academix-89443",
-  storageBucket: "academix-89443.firebasestorage.app",
-  messagingSenderId: "1095602054525",
-  appId: "1:1095602054525:web:d3509c4f0af027973d5814"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
+
